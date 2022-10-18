993b757f16388043d6332029537ef1e1c1d5f6c9
Erickian2007 <erickian2007@gmail.com>
2022-10-17 23:06:48 -0300

    update

--- menu.gd
+++ menu.gd
@@ -2,13 +2,24 @@ extends CanvasLayer
 
 onready var http_request: HTTPRequest = get_node("HTTPRequest")
 
+var can_refresh: bool
+
 func _ready() -> void:
-	http_request.connect("request_completed", self, "_request_completed")
+	var _connect_request_completed = http_request.connect("request_completed", self, "_request_completed")
 	
+func _process(_delta):
+	if IP.get_local_addresses() != null:
+		$Menu/Infor/Conection.text = ""
+		can_refresh = true
+	else:
+		$Menu/Infor/Conection.text = "SEM CONEXÃO"
+		can_refresh = false
+		
 func _on_refresh_pressed() -> void:
-	http_request.request("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if can_refresh:
+		var _request = http_request.request("https://economia.awesomeapi.com.br/json/last/USD-BRL")
 	
-func _request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
+func _request_completed(_result: int, _response_code: int, _headers: PoolStringArray, body: PoolByteArray) -> void:
 	var json = JSON.parse(body.get_string_from_utf8())
 	if json.error == OK:
 		var value = json.get_result()
