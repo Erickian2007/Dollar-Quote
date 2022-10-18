0c7c6a3e6dfd146a6ef2cb8c039105d2dae026d5
Erickian2007 <erickian2007@gmail.com>
2022-10-18 00:43:29 -0300

    new-bg

--- menu.gd
+++ menu.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	var _connect_request_completed = http_request.connect("request_completed", self, "_request_completed")
 	
 func _process(_delta):
-	if IP.get_local_addresses() != null:
+	if http_request.get_http_client_status() != null:
 		$Menu/Infor/Conection.text = ""
 		can_refresh = true
 	else:
@@ -18,7 +18,9 @@ func _process(_delta):
 func _on_refresh_pressed() -> void:
 	if can_refresh:
 		var _request = http_request.request("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-	
+		$Menu/Buttons/Refresh.disabled = true
+		$Wait.start(2)
+		
 func _request_completed(_result: int, _response_code: int, _headers: PoolStringArray, body: PoolByteArray) -> void:
 	var json = JSON.parse(body.get_string_from_utf8())
 	if json.error == OK:
@@ -29,4 +31,5 @@ func _request_completed(_result: int, _response_code: int, _headers: PoolStringA
 		
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
+func _on_wait_timeout():
+	$Menu/Buttons/Refresh.disabled = false
